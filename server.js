decc3d5b0cca10aafbab1cc46eb67853aba7406d
Viivi <viivi@example.com>
2020-03-09 20:42:29 +0200

    commit2

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/', function(req, res){
    });
    
 });
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000, function(){
     console.log("Server is running on port 5000");
 });
 
